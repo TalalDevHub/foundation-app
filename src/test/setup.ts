f6b3713f883e9e2928b8f21a1251bed8611e2a2d
Talal Shah <imtalal21@gmail.com>
2026-09-15 08:32:58 +0500

    fix(build): clean tsconfig encoding and isolate test suites for green ci/cd

--- src/test/setup.ts
+++ src/test/setup.ts
@@ -1,4 +1,5 @@
-﻿import "@testing-library/jest-dom";
+﻿/// <reference types="@testing-library/jest-dom" />
+import "@testing-library/jest-dom";
 
 // Polyfill window.HTMLElement scroll behaviors for JSDOM
 HTMLElement.prototype.scrollTo = () => {};
